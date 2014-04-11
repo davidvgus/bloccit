158d921a5bb6a93d0b367551fe117ae3a9053001
David Gustafson <git@radicalskeptic.net>
2014-04-11 14:07:03 -0700

    Cleans up registration views and layout.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    @post = current_user.posts.build(post_params)
     @post.topic = @topic
 
     authorize @post
@@ -38,10 +38,16 @@ class PostsController < ApplicationController
     authorize @post
     if @post.update_attributes(params.require(:post).permit(:title, :body))
       flash[:notice] = "Post was updated."
-      redirect_to [@tipic, @post]
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :edit
     end
+  end
+
+private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
