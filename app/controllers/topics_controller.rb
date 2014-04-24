0cb33e49e3b4d101234f88f45bbbd7a2e88ac08f
David Gustafson <git@radicalskeptic.net>
2014-04-23 20:39:33 -0700

    Created Popular Posts and Users, and optimized topics#show with eager loading.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -13,7 +13,9 @@ class TopicsController < ApplicationController
   def show
     @topic = Topic.find(params[:id])
     authorize @topic
-    @posts = @topic.posts.paginate(page: params[:page], per_page: 10)
+    #@posts = @topic.posts.paginate(page: params[:page], per_page: 10)
+    #@posts = @topic.posts.includes(:user).paginate(page: params[:page], per_page: 10)
+    @posts = @topic.posts.includes(:user).includes(:comments).paginate(page: params[:page], per_page: 10)
   end
 
   def edit
