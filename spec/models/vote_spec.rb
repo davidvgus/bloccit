afa35a20f31b1bf0c9f9c65ca27c7dc7716ddb94
David Gustafson <git@radicalskeptic.net>
2014-04-24 13:27:52 -0700

    removed some spec files

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 describe Vote do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#up_vote?" do
+    it "returns true for an up vote" do
+      v = Vote.new(value: 1)
+      v.up_vote?.should be_true
+    end
+    it "returns false for a down vote" do
+      v = Vote.new(value: -1)
+      v.up_vote?.should be_false
+    end
+  end
+
+  describe "#down_vote?" do
+    it "returns true for a down vote" do
+      v = Vote.new(value: -1)
+      v.down_vote?.should be_true
+    end
+    it "returns false for an up vote" do
+      v = Vote.new(value: 1)
+      v.down_vote?.should be_false
+    end
+  end
+
+  describe "#update_post" do
+    it "calls `update_rank` on post" do
+      post = Post.new
+      post.save(validate: false)
+      post.should_receive(:update_rank)
+      Vote.create(value: 1, post: post)
+    end
+  end
+
 end
